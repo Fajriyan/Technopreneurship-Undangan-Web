751efb072f4193807c476b1900c846397b073072
Fajriyan <fajriyan20@gmail.com>
2020-06-16 22:47:26 +0700

    UJIAN AKHIR SEMESTER

---
+++ application/views/front/recommended.php
@@ -0,0 +1,58 @@
+<div class="recommended_items">
+	<!--recommended_items-->
+	<h2 class="title text-center">recommended items</h2>
+
+	<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
+		<div class="carousel-inner">
+			<div class="item active">
+				<div class="col-sm-4">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+							<div class="productinfo text-center">
+								<img width="100px" height="180px" src="<?php echo base_url() ?>assets/front/images/home/rekom-1.jpg" alt="" />
+								<h2>$14</h2>
+								<p>Rekomendasai 1</p>
+								<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+							<div class="productinfo text-center">
+								<img width="100px" height="180px" src="<?php echo base_url() ?>assets/front/images/home/rekom2.jpg" alt="" />
+								<h2>$34</h2>
+								<p>Rekomendasi 2</p>
+								<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="product-image-wrapper">
+						<div class="single-products">
+							<div class="productinfo text-center">
+								<img width="100px" height="180px" src="<?php echo base_url() ?>assets/front/images/home/rekom3.jpg" alt="" />
+								<h2>$28</h2>
+								<p>Rekomendasi 3</p>
+								<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
+			<i class="fa fa-angle-left"></i>
+		</a>
+		<a class="right recommended-item-control" href="#recommended-item-carousel" data-slide="next">
+			<i class="fa fa-angle-right"></i>
+		</a>
+	</div>
+</div>
+<!--/recommended_items-->
